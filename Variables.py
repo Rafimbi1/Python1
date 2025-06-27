fd215b071b25ea3f058fed5ae742bc39ec901c58
Phoebe Rafimbi <adikinyirafimbi@gmail.com>
2025-06-27 10:16:53 +0300

    Created Variables file

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# Variables example
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "meters")
+print("Are you a student?", is_student)
