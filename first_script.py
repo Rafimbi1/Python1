4be945153cd46cf7eb000f6061df4dfe1ac53a2a
Phoebe Rafimbi <adikinyirafimbi@gmail.com>
2025-06-27 10:02:55 +0300

    created our first file

---
+++ first_script.py
@@ -0,0 +1,8 @@
+# This is your first Python script
+
+print("Hello, Phoebe!")
+print("Welcome to your Python journey 🚀")
+
+name = input("What's your name? ")
+age = input("whats your age?")
+print("Nice to meet you, " + name + "! You are " + age + " years old.")
