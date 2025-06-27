d09dd1a028f0a2af6886ab6f6fa52c547e305bb3
Phoebe Rafimbi <adikinyirafimbi@gmail.com>
2025-06-27 10:37:21 +0300

    Create Practice file

---
+++ Practice.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+birth_year = input ("what year were you born?")
+
+age = age = 2025 - int(birth_year)
+print("Nice to meet you, " + name + "! You are " + str(age) + " years old.")
+print("Data types:")
+print("Name:", type(name))
+print("Birth year:", type(birth_year))
+print("Age:", type(age))
